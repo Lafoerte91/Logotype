c4830a6670098498ff143d178c9821060cd136a4
Lafoerte91 <lafoerte@gmail.com>
2024-05-24 14:43:55 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -32,4 +32,58 @@ window.addEventListener('DOMContentLoaded', function() {
       showTabContent(index) // показываем нужное содержимое
     }
   })
+
+  // Timer
+  const deadline = '2024-06-11' // дата окончания
+
+  function getTimerRemaining(endtime) { // получаем разницу между датами
+    const t = Date.parse(endtime) - Date.parse(new Date()) // разница между датами
+    const days = Math.floor(t / (1000 * 60 * 60 * 24)) // дни
+    const hours = Math.floor((t / 1000 * 60 * 60) % 24) // часы
+    const minutes  = Math.floor((t / 1000 / 60) % 60) // минуты
+    const seconds  = Math.floor((t / 1000) % 60) // секунды
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    }
+  }
+
+  function getZero(num) { // приводим число к двухзначному виду
+    if (num >= 0 && num < 10) {
+      return `0${num}`
+    } else {
+      return num
+    }
+  }
+
+  function setClock(selector, endtime) { // устанавливаем таймер
+    const timer = document.querySelector(selector) // получаем таймер
+    const days = timer.querySelector('#days') 
+    const hours = timer.querySelector('#hours') 
+    const minutes  = timer.querySelector('#minutes') 
+    const seconds  = timer.querySelector('#seconds')
+
+    let timeInterval = setInterval(updateClock, 1000) // устанавливаем интервал обновления
+    updateClock() // обновляем таймер
+
+    function updateClock() { // функция обновления таймера
+      const t = getTimerRemaining(endtime) // получаем разницу между датами
+      days.textContent = getZero(t.days) // показываем дни
+      hours.textContent = getZero(t.hours) // показываем часы
+      minutes.textContent = getZero(t.minutes) // показываем минуты
+      seconds.textContent = getZero(t.seconds) // показываем секунды
+
+      if(t.total <= 0) { // если время закончилось
+        clearInterval(timeInterval) // останавливаем интервал обновления
+      }
+    }
+  }
+
+  setClock('.timer', deadline) // устанавливаем таймер
 })
+
+
