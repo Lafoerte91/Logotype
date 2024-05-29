740a97a98648ad600b306de8421076633cb4619d
Lafoerte91 <lafoerte@gmail.com>
2024-05-29 11:51:08 +0300

    ...rest

--- js/script.js
+++ js/script.js
@@ -144,12 +144,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
   // Classes
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src
       this.alt = alt
       this.title = title
       this.descr = descr
       this.price = +price
+      this.classes = classes
       this.parent = document.querySelector(parentSelector)
       this.transfer = 91
       this.changeToRUB()
@@ -159,8 +160,13 @@ window.addEventListener('DOMContentLoaded', function() {
     }
     render() {
       const element = document.createElement('div')
+      if(this.classes.length === 0) {
+        this.element = 'menu__item'
+        element.classList.add(this.element)
+      } else {
+        this.classes.forEach(className => element.classList.add(className))
+      }
       element.innerHTML = `
-    <div class="menu__item">
       <img src="${this.src}" alt="${this.alt}">
       <h3 class="menu__item-subtitle">${this.title}</h3>
       <div class="menu__item-descr">${this.descr}</div>
@@ -169,7 +175,6 @@ window.addEventListener('DOMContentLoaded', function() {
           <div class="menu__item-cost">Цена:</div>
           <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
       </div>
-    </div>
       `
       this.parent.append(element)
     }
@@ -190,7 +195,8 @@ window.addEventListener('DOMContentLoaded', function() {
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     6,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render()
 
   new MenuCard(
@@ -199,7 +205,8 @@ window.addEventListener('DOMContentLoaded', function() {
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     4,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render()
 })
 
